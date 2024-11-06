b95a996d62af3ca228d2815f13365ebd16af06b7
Alexander Masztak <itzalqxander@gmail.com>
2024-11-06 15:24:59 -0600

    misc

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import Box from './components/Box';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
